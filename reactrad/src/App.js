10fb2d9780468a32a9dc325801e6ee2393b7bdcd
Bohdan Malish <77528085+BohdanMalish@users.noreply.github.com>
2021-06-20 10:42:56 +0300

    first

---
+++ reactrad/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import ListItem from './components/listItems/items';
+function App() {
+  const l={
+    name:'ki',
+    context:'lo',
+    category:'task'
+  }
+  return (
+    <div >
+   <ListItem></ListItem>
+   
+    </div>
+  );
+}
+
+export default App;
